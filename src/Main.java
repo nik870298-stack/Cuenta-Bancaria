b7bca40daa8585c2a9e664fe6cc7f10702a31f44
Nikolas R <nik870298@gmail.com>
2025-09-19 17:58:34 -0500

    Avance2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        cuentaBancaria count1 = new cuentaBancaria(11002251, 1122513880, 11000);
-        System.out.println(count1.toString());
-        System.out.println(count1.ingresarDinero(count1.getsaldoActual()));
+        cuentaBancaria.menu();
     }
 }
--- src/cuentaBancaria.java
+++ src/cuentaBancaria.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class cuentaBancaria {
     //Atributos de la clase
 
-    private int numeroDeCuenta;
+    private double numeroDeCuenta;
     private double cedulaDeCliente;
     private double saldoActual;
 
@@ -14,7 +14,7 @@ public class cuentaBancaria {
 
     //  Constructor de todos los parametros
 
-    public cuentaBancaria(int numeroDeCuenta, double cedulaDeCliente, double saldoActual) {
+    public cuentaBancaria(double numeroDeCuenta, double cedulaDeCliente, double saldoActual) {
         this.numeroDeCuenta = numeroDeCuenta;
         this.cedulaDeCliente = cedulaDeCliente;
         this.saldoActual = saldoActual;
@@ -24,7 +24,7 @@ public class cuentaBancaria {
         return numeroDeCuenta;
     }
 
-    public void setNumeroDeCuenta(int numeroDeCuenta) {
+    public void setNumeroDeCuenta(double numeroDeCuenta) {
         this.numeroDeCuenta = numeroDeCuenta;
     }
 
@@ -55,15 +55,86 @@ public class cuentaBancaria {
     }
 
     //Metodos Propios
+
+    //Metodo Ingresar Dinero
     public double ingresarDinero(double n1){
-        Scanner ingreso = new Scanner(System.in);
-        System.out.println("Su saldo actual es: "+saldoActual);
-        System.out.println("Digite la cantidad de dinero que quiere ingresar: ");
-        n1 = ingreso.nextDouble();
-        saldoActual+=n1;
-        System.out.println("Su nuevo saldo es: "+saldoActual);
-        return 0;
+        if (n1<0){
+            System.out.println("No es posible ingresar ese monto");
+        }
+        return this.saldoActual+=n1;
     }
 
+    //Retirar dinero
+    public double retirarDinero(double n1){
+        if(n1>saldoActual){
+            System.out.println("Fondos insuficientes de la cuenta");
+        }
+        return this.saldoActual-=n1;
+    }
 
+    //Extraccion rapida
+    public double retiroRapido(double n1){
+        if(saldoActual<10000){
+            System.out.println("La cuenta no tiene fondos");
+        }
+        n1 = 0.20;
+        return this.saldoActual*n1;
+    }
+
+    //Consultar saldo
+    public double consultarSaldo(){
+        System.out.println("Tu saldo actual es de: ");
+        return this.saldoActual;
+    }
+
+    //Menu
+    public static void menu(){
+        Scanner leer = new Scanner(System.in);
+        double monto;
+        cuentaBancaria count1 = new cuentaBancaria();
+        System.out.println("Digite su numero de cuenta: ");
+        count1.setNumeroDeCuenta(leer.nextDouble());
+        System.out.println("Digite su numero de cliente(cedula): ");
+        count1.setCedulaDeCliente(leer.nextDouble());
+        count1.setsaldoActual(10000);
+        do{
+            System.out.println("-----------------------Cuenta Bancaria--------------------");
+            System.out.println("Datos de su cuenta: "+count1.toString());
+            System.out.println("¿Que tramite quiere hacer?");
+            System.out.println("1. Ingresar Dinero");
+            System.out.println("2. Retirar Dinero");
+            System.out.println("3. Extraccion Rapida Rapido");
+            System.out.println("4. Consultar saldo");
+            System.out.println("5. Salir");
+            System.out.println("Escriba la opcion que desee: ");
+
+
+            int opcion = leer.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.println("Su saldo es: ");
+                    System.out.println(count1.ingresarDinero(count1.getsaldoActual()));
+                    System.out.println("Ingrese el monto que quiere ingresar: ");
+                    monto = leer.nextDouble();
+                    count1.ingresarDinero(monto);
+                    break;
+                case 2:
+                    System.out.println("Su saldo es: ");
+                    System.out.println(count1.retirarDinero(count1.getsaldoActual()));
+                    System.out.println("Ingrese el monto que quiere retirar: ");
+                    monto = leer.nextDouble();
+                    count1.retirarDinero(monto);
+                    break;
+                case 3:
+                    System.out.println(count1.retiroRapido(count1.getsaldoActual()));
+                    break;
+                case 4:
+                    System.out.println(count1.consultarSaldo());
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+            }
+        }while(true);
+    }
 }
